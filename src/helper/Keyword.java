43e602604994e87de771118d06c39e82059f489d
Shai Mann-Robison <mann.robison@gmail.com>
2018-05-07 10:04:36 -0400

    Add Keyword Class

--- src/helper/Keyword.java
+++ src/helper/Keyword.java
@@ -1,5 +1,26 @@
 package helper;
 
 public class Keyword {
-
+	
+	public static final String PRINT = "print";
+	public static final String PRINT_LINE = "println";
+	public static final String NEW = "new";
+	
+	public static final String FOR_LOOP = "for";
+	public static final String WHILE_LOOP = "while";
+	public static final String IF_STATEMENT = "if";
+	public static final String ELIF_STATEMENT = "elif";
+	
+	public static final String ADD = "+";
+	public static final String SUBTRACT = "-";
+	public static final String MULTIPLY = "*";
+	public static final String DIVIDE = "/";
+	
+	public static final String OPEN_PERENTHESIS = "(";
+	public static final String CLOSE_PERENTHESIS = ")";
+	public static final String OPEN_CURLY_BRACES = "{";
+	public static final String CLOSE_CURLY_BRACES = "}";
+	
+	public static final String END_LINE = "\n";
+	
 }

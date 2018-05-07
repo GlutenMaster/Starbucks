b34da6df14882d963175144773ca9ca7fa6997ad
Shai Mann-Robison <mann.robison@gmail.com>
2018-05-07 15:24:05 -0400

    Add Swing Compatibility

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,67 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
 
-import javax.swing.JLabel;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		 System.out.println("Enter the paths for the input and output files");
-		 Scanner inputPath = new Scanner(System.in);
-		 Scanner outputPath = new Scanner(System.in);
-		 String inputPathString = new String (inputPath.nextLine());
-		 String outputPathString = new String (outputPath.nextLine());
-		 outputPath.close();
-		 inputPath.close();
-		 System.out.println(inputPathString);
-		 System.out.println(outputPathString);
-	}
 	
+	private static JFrame frame;
+	private static JTextField inputField;
+	private static JTextArea outputField;
+	private static JPanel panel;
+	
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		 initFrame();
+	}
+	private static void addActionListener() {
+		inputField.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+            	if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                	
+                }
+            }
+        });
+	}
+	private static void initFrame() {
+        frame = new JFrame("The Greatest IDE of all time LoL");
+
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        
+        inputField = new JTextField();
+        inputField.setPreferredSize(new Dimension(500, 50));
+        inputField.setBorder(new TitledBorder(""));
+        panel.add(inputField);
+        outputField = new JTextArea();
+        outputField.setBorder(new TitledBorder(""));
+        outputField.setPreferredSize(new Dimension(800, 200));
+        outputField.setEditable(false);
+        outputField.setText("LoL ur bad");
+        panel.add(outputField);
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.revalidate();
+        frame.repaint();
+        
+        addActionListener();
+    }
+	private static void print(String text) {
+		outputField.setText(outputField.getText() + "\n" + text);
+		frame.pack();
+		frame.revalidate();
+		frame.repaint();
+	}
 }
--- src/helper/Keyword.java
+++ src/helper/Keyword.java
@@ -2,6 +2,9 @@ package helper;
 
 public class Keyword {
 	
+	public static final String CLASS = "class";
+	public static final String STATIC = "static";
+	
 	public static final String PRINT = "print";
 	public static final String PRINT_LINE = "println";
 	public static final String NEW = "new";
@@ -10,16 +13,19 @@ public class Keyword {
 	public static final String WHILE_LOOP = "while";
 	public static final String IF_STATEMENT = "if";
 	public static final String ELIF_STATEMENT = "elif";
+	public static final String ELSE_STATEMENT = "else";
+	public static final String SWITCH_STATEMENT = "switch";
+	public static final String CASE_STATEMENT = "case";
 	
 	public static final String ADD = "+";
 	public static final String SUBTRACT = "-";
 	public static final String MULTIPLY = "*";
 	public static final String DIVIDE = "/";
 	
-	public static final String OPEN_PERENTHESIS = "(";
-	public static final String CLOSE_PERENTHESIS = ")";
-	public static final String OPEN_CURLY_BRACES = "{";
-	public static final String CLOSE_CURLY_BRACES = "}";
+	public static final String OPEN_ARGUMENTS = "(";
+	public static final String CLOSE_ARGUMENTS = ")";
+	public static final String OPEN_CODE_BLOCK = ":";
+	public static final String END_CODE_BLOCK = ";";
 	
 	public static final String END_LINE = "\n";
 	
